06537fea39781d8a7362f839fc1b02847fee8d21
= <=>
2024-06-21 18:42:53 +0530

    [feat] : added product service

--- src/app/Components/Dashboard/home/home.component.ts
+++ src/app/Components/Dashboard/home/home.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../Services/Product/product.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerLoadingComponent } from '../../Common/spinner-loading/spinner-loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SpinnerLoadingComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  products: any[] = [];
 
+  constructor(
+    private productService: ProductService,
+    private spinner: NgxSpinnerService
+  ) {}
+
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.productService.getAllProducts().subscribe(
+        (data: any) => {
+          this.products = data;
+          console.log('data', data);
+          this.spinner.hide();
+        },
+        (error: any) => {
+          console.error('Error fetching products:', error);
+          this.spinner.hide();
+        }
+      );
+    }, 2000);
+  }
 }
---
+++ src/app/Services/Product/product.service.ts
@@ -0,0 +1,34 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:8888/api/v1/product';
+
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<any[]> {
+    return this.http
+      .get<any[]>(`${this.apiUrl}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
