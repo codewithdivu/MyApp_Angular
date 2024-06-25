49d32abd95ad0928da09295adc52d4cd66418ebb
= <=>
2024-06-25 14:46:07 +0530

    [fixed : state constant

--- src/app/Services/Auth/auth.service.ts
+++ src/app/Services/Auth/auth.service.ts
@@ -6,60 +6,66 @@ import {
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { API_ROUTES, HOST_API } from '../../Constants/apiRoutes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8888/api/v1/auth';
 
   constructor(private http: HttpClient) {}
 
   login(loginData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, loginData).pipe(
-      map((res: any) => {
-        if (res.success) {
-          localStorage.setItem('accessToken', res.accessToken);
-          localStorage.setItem('myAppAuth', JSON.stringify(res.data));
-        }
-        return res;
-      }),
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http
+      .post(`${HOST_API}${API_ROUTES.AUTH.LOGIN}`, loginData)
+      .pipe(
+        map((res: any) => {
+          if (res.success) {
+            localStorage.setItem('accessToken', res.accessToken);
+            localStorage.setItem('myAppAuth', JSON.stringify(res.data));
+          }
+          return res;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
   }
 
   register(signUpData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, signUpData).pipe(
-      map((res: any) => {
-        if (res.success) {
-        }
-        return res;
-      }),
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http
+      .post(`${HOST_API}${API_ROUTES.AUTH.REGISTRATION}`, signUpData)
+      .pipe(
+        map((res: any) => {
+          if (res.success) {
+          }
+          return res;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
   }
 
   forgotPassword(email: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/forgot-password`, { email }).pipe(
-      map((res: any) => {
-        if (res.success) {
-          localStorage.setItem('forgotPasswordEmail', email);
-        }
-        return res;
-      }),
-      catchError((error: HttpErrorResponse) => {
-        return throwError(error);
-      })
-    );
+    return this.http
+      .post(`${HOST_API}${API_ROUTES.AUTH.FORGOT_PASSWORD}`, { email })
+      .pipe(
+        map((res: any) => {
+          if (res.success) {
+            localStorage.setItem('forgotPasswordEmail', email);
+          }
+          return res;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
   }
 
   resetPassword(forgotPasswordData: any): Observable<any> {
     return this.http
-      .post(`${this.apiUrl}/reset-password`, forgotPasswordData)
+      .post(`${HOST_API}${API_ROUTES.AUTH.RESET_PASSWORD}`, forgotPasswordData)
       .pipe(
         map((res: any) => {
           if (res.success) {
--- src/store/app.state.ts
+++
@@ -1,6 +0,0 @@
-
-import { CartState } from './reducers/cart.reducer';
-
-export interface AppState {
-  cart: CartState;
-}
